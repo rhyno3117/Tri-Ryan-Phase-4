9abdd13498cc1b7ef4b44443b4a5c59dc5f0763c
ryan <rhynogolfer42@gmail.com>
2024-01-03 14:13:43 -0800

    UpdatedLanding

--- client/src/components/Pages/Landing.jsx
+++ client/src/components/Pages/Landing.jsx
@@ -18,33 +18,45 @@ function Landing() {
           justifyContent: 'center',
           padding: '20px 0',
           height: '80vh',
-          marginTop:'-25vh'
+          marginTop: '-25vh'
         }}
       >
         <div
           style={{
             textAlign: 'center',
-            marginTop: '200px', // Adjust the margin-top to move the text lower
+            marginTop: '300px', // Adjust the margin-top to move the text lower
             fontFamily: 'Impact, Haettenschweiler, "Arial Narrow Bold", sans-serif',
             fontSize: '40px',
           }}
         >
           <p id="text">
-            This website has been created for you.<br/>
-            We help find you events to go to.<br/>
+            This website has been created for you.<br />
+            We help find you events to go to.<br />
             We give you the capability to add and RSVP events.
           </p>
-          <button onClick={handleGetStarted} style={{borderRadius:'100px', backgroundColor:'#2e9bd6', color:'#white',padding:'10px'}}>Get Started</button>
+          <button 
+          onClick={handleGetStarted} 
+          style={{
+            borderRadius: '100px',
+            backgroundColor: '#2e9bd6',
+            color: '#white',
+            padding: '10px'
+          }}>Get Started
+          </button>
         </div>
       </div>
       <div className="background-image">
         <img
           src="https://miro.medium.com/v2/resize:fit:1358/1*Dmw3hv_CHbZEPyFTayw8Yw.png"
           alt="Background Party Image"
-          style={{ width: '100%', height: 'auto'}}
+          style={{ width: '100%', 
+          height: 'auto',
+          marginBottom: '0',
+
+        }}
         />
       </div>
-      </>
+    </>
   );
 }
 
